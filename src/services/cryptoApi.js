bd1e2bd46ccf0bd8c991900aaa4862ad7e540241
ddao19 <dung.dao19@outlook.com>
2021-11-14 17:03:20 -0600

    Added API and redux toolkit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "a4bc3e2ef7msh0f767d9b0fdf469p1117f0jsn66b5fb5b5ca7",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/exchanges";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
